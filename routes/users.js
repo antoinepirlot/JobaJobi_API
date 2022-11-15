35bb239a02c085fe5b3102d712ff9efacd862eda
Stefan <stefan.mircovici@student.vinci.be>
2022-11-15 12:17:16 +0100

    [ADD] getUserFromSession route and model fuction and http test and authorize file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const { authorize } = require('../utils/authorize');
+const { Users } = require("../model/users");
+const userModel = new Users();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/* GET user of the session. */
+router.get('/getUserSession/', authorize,function(req, res, next) {
+  const result =  userModel.getUserFromSession(req.user);
+  res.send(result);
 });
 
 module.exports = router;
